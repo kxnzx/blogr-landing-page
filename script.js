758dc0c7e3a04c9d17db956a8ec1641f17ecbb47
“kxnzx” <k.bousboa@gmail.com>
2023-03-19 15:27:42 +0100

    edit

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ const openIcon = document.querySelector(".openIcon");
 // FUNCTION
 function toggleMenu() {
   // If it's true that the menu class contains the showMenu class, do this:
-  if (menu.classList.contains("showMenu")) {
+  if (menu.classList.contains("displayMenu")) {
     // Execute this block of code if the specified condition is true:
-    menu.classList.remove("showMenu");
+    menu.classList.remove("displayMenu");
     closeIcon.style.display = "none";
     openIcon.style.display = "block";
   }
   // If it's false that the menu class contains the showMenu class, do this:
   else {
     // Execute this block of code if the specified condition is false:
-    menu.classList.add("showMenu");
+    menu.classList.add("displayMenu");
     closeIcon.style.display = "block";
     openIcon.style.display = "none";
   }
